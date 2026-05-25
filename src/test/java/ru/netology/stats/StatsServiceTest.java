15196a46f4e6b4271b0cb9b47f61980fb82e5c05
Nikitos <nikita4651@yandex.ru>
2026-05-25 12:08:17 +0300

    test: migrate StatsServiceTest to JUnit5

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -76,6 +76,6 @@ public class StatsServiceTest {
         long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 15;
         int actual = service.averageSales(sales);
-        Assertions.assertEquals(expected, actual, 1);
+        Assertions.assertEquals(expected, actual);
     }
 }
